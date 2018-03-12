c055fd6f25c209d936007ff64cf513f283c20132
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-12 17:39:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,15 @@
-def display_board(board)
+def valid_move?(board, x)
+  if x.between?(0,8) && !position_taken?(board, x)
+    true
+  else
+    false
+  end
+end
 
+def position_taken?(board, x)
+  if board[x] == " " || board[x] == "" || board[x] == nil
+    false
+  elsif board[x] == "X" || board[x] == "O"
+    true
+  end
 end
